14f1357fc648be1564d696485bbf9112b7e44298
MariaRadchuk <mradchuk_15@ukr.net>
2024-02-24 18:37:30 +0200

    new

--- main.js
+++ main.js
@@ -1,45 +1,50 @@
-const allBooks = [
-  { title: 'Книга 1', category: 'Пригоди' },
-  { title: 'Книга 2', category: 'Фантастика' },
-  { title: 'Книга 3', category: 'Романи' },
-  { title: 'Книга 4', category: 'Пригоди' },
-];
+import './js/api';
+import './js/render-functions';
+import './js/themechange';
+import './js/mob-menu';
 
-const booksList = document.getElementById('booksList');
-const categoryLinks = document.querySelectorAll('.category'); // Отримуємо всі посилання категорій
-const allBooksLink = document.getElementById('allBooks'); // Отримуємо посилання на "Всі книги"
+// const allBooks = [
+//   { title: 'Книга 1', category: 'Пригоди' },
+//   { title: 'Книга 2', category: 'Фантастика' },
+//   { title: 'Книга 3', category: 'Романи' },
+//   { title: 'Книга 4', category: 'Пригоди' },
+// ];
 
-function displayBooks(books) {
-  booksList.innerHTML = '';
+// const booksList = document.getElementById('booksList');
+// const categoryLinks = document.querySelectorAll('.category'); // Отримуємо всі посилання категорій
+// const allBooksLink = document.getElementById('allBooks'); // Отримуємо посилання на "Всі книги"
 
-  books.forEach(book => {
-    const bookItem = document.createElement('div');
-    bookItem.textContent = book.title;
-    booksList.appendChild(bookItem);
-  });
-}
+// function displayBooks(books) {
+//   booksList.innerHTML = '';
 
-// Функція для відображення книг конкретної категорії
-function displayBooksByCategory(event) {
-  event.preventDefault(); // Зупиняємо дію переходу за посиланням
-  const category = this.textContent; // Отримуємо текст категорії
-  const booksInCategory = allBooks.filter(book => book.category === category); // Фільтруємо книги за категорією
-  displayBooks(booksInCategory); // Відображаємо книги конкретної категорії
-}
+//   books.forEach(book => {
+//     const bookItem = document.createElement('div');
+//     bookItem.textContent = book.title;
+//     booksList.appendChild(bookItem);
+//   });
+// }
 
-// Функція для відображення всіх книг
-function displayAllBooks(event) {
-  event.preventDefault(); // Зупиняємо дію переходу за посиланням
-  displayBooks(allBooks); // Відображаємо всі книги
-}
+// // Функція для відображення книг конкретної категорії
+// function displayBooksByCategory(event) {
+//   event.preventDefault(); // Зупиняємо дію переходу за посиланням
+//   const category = this.textContent; // Отримуємо текст категорії
+//   const booksInCategory = allBooks.filter(book => book.category === category); // Фільтруємо книги за категорією
+//   displayBooks(booksInCategory); // Відображаємо книги конкретної категорії
+// }
 
-// Додаємо прослуховувач подій для кожної категорії
-categoryLinks.forEach(categoryLink => {
-  categoryLink.addEventListener('click', displayBooksByCategory);
-});
+// // Функція для відображення всіх книг
+// function displayAllBooks(event) {
+//   event.preventDefault(); // Зупиняємо дію переходу за посиланням
+//   displayBooks(allBooks); // Відображаємо всі книги
+// }
 
-// Додаємо прослуховувач подій для "Всі книги"
-allBooksLink.addEventListener('click', displayAllBooks);
+// // Додаємо прослуховувач подій для кожної категорії
+// categoryLinks.forEach(categoryLink => {
+//   categoryLink.addEventListener('click', displayBooksByCategory);
+// });
 
-// Відображення всіх книг при завантаженні сторінки
-displayBooks(allBooks);
+// // Додаємо прослуховувач подій для "Всі книги"
+// allBooksLink.addEventListener('click', displayAllBooks);
+
+// // Відображення всіх книг при завантаженні сторінки
+// displayBooks(allBooks);
